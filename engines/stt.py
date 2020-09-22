3f3a02ec5490f2ef46d031b4c10cc8ca9aa2e553
jose <josmejia.2401@gmail.com>
2020-09-22 14:59:39 -0500

    Renombrado de clases

---
+++ engines/stt.py
@@ -0,0 +1,87 @@
+import speech_recognition as sr
+from observerx.observerx import ConcreteSubject
+import time
+import threading
+
+class STTEngine(ConcreteSubject):
+    """
+    Speech To Text Engine (STT)
+
+    Google API Speech recognition settings
+    SpeechRecognition API : https://pypi.org/project/SpeechRecognition/2.1.3
+    """
+
+    def __init__(self):
+        super().__init__() 
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+        # energía de audio mínima a considerar para la grabación
+        self.recognizer.energy_threshold = 4000
+        # segundos de audio sin hablar antes de que una frase se considere completa
+        self.recognizer.pause_threshold = 0.5
+        self.recognizer.dynamic_energy_threshold = True
+        # segundos mínimos de audio hablado antes de que consideremos el audio hablado como una frase; los valores por debajo de esto se ignoran (para filtrar clics y estallidos)
+        #self.recognizer.phrase_threshold = 0.3
+        # segundos de audio que no habla para mantenerse en ambos lados de la grabación
+        #self.recognizer.non_speaking_duration = 0.2
+
+        self.main_lang = "es-ES"
+        self.keyword_lang = "es"
+        self.keywords = [("cristal", 1), ("hey cristal", 1), ("ey cristal", 1), ("christal", 1)]
+
+    def run(self, already_activated=False):
+        if not isinstance(self.recognizer, sr.Recognizer):
+            raise TypeError("`recognizer` must be `Recognizer` instance")
+        if not isinstance(self.microphone, sr.Microphone):
+            raise TypeError("`microphone` must be `Microphone` instance")
+        self.recognize_input(already_activated)
+        
+    def stop(self):
+        print("Deteniendo...")
+
+    def recognize_input(self, already_activated=False):
+        response = self._recognize_speech_from_mic(already_activated)
+        self.stop()
+        self.some_business_logic(response)
+
+    def _recognize_speech_from_mic(self, already_activated=False):
+        print("escuchando")
+        response = { "success": True, "error": None, "transcription": None }
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source, duration=0.5)
+            audio = self.recognizer.listen(source)
+        try:
+            #speech_as_text = recognizer.recognize_sphinx(audio, keyword_entries=self.keywords, language=self.keyword_lang)
+            response["transcription"] = self.recognizer.recognize_google(audio, language=self.main_lang).lower()
+            if already_activated == False and self._activation_name_exist(response):
+                response = self._remove_activation_word(response)
+            elif already_activated == True:
+                pass
+            else:
+                response["success"] = False
+        except sr.UnknownValueError:
+            response["success"] = False
+            response["transcription"] = "Función no conocida"
+        except sr.RequestError:
+            response["success"] = False
+            response["transcription"] = "API no disponible en este momento."
+        return response
+
+    #@staticmethod
+    def _activation_name_exist(self, transcript):
+        if transcript["transcription"]:
+            transcript_words = transcript["transcription"].split()
+            names = []
+            for name in self.keywords:
+                names.append(name[0])
+            return bool(set(transcript_words).intersection(names))
+        else:
+            return False
+
+    #@staticmethod
+    def _remove_activation_word(self, transcript):
+        for name in self.keywords:
+            count = transcript["transcription"].count(name[0])
+            transcript["transcription"] = transcript["transcription"].replace(name[0], "", count)
+        transcript["transcription"] = transcript["transcription"].strip()
+        return transcript
--- main.py
+++ main.py
@@ -1,102 +1,42 @@
 #!/usr/bin/env python3
-import threading
-from eventsx.listen import Listen
-from eventsx.speak import Speak
+from engines.stt import STTEngine
 from observerx.observerx import Observer
-from category.mex import MeCategory
-from category.joke import JokeCategory
-from category.alarm import AlarmCategory
-from category.timex import TimeCategory
-
+"""
+Espera unos segundos
+Cristal listo!!! estoy escuhando.
+"""
 class Cristal(Observer):
 
     def __init__(self):
         super().__init__()
-        self.listen = Listen()
-        self.speak = Speak()
-        self.HISTORY = []
-        self.CURRENT_CONTEXT = None
-        #category
-        self.me_category = MeCategory()
-        self.joke_category = JokeCategory()
-        self.alarm_category = AlarmCategory()
-        self.time_category = TimeCategory()
+        self.sTTEngine = STTEngine()
+        self.build()
     
-    def get_model_path(self) -> str:
-        from pocketsphinx import get_model_path
-        model_path = get_model_path()
-        return model_path
+    def build(self):
+        self.sTTEngine.attach(self)
 
     def run(self):
         try:
-            self.listen.attach(self)
-            self.listen.run()
+            self.sTTEngine.run(already_activated=False)
         except KeyboardInterrupt as e:
-            self.me_category.stop()
-            self.joke_category.stop()
-            self.alarm_category.stop()
-            self.time_category.top()
+            print(e)
+            self.sTTEngine.stop()
         except Exception as e:
-            self.me_category.stop()
-            self.joke_category.stop()
-            self.alarm_category.stop()
-            self.time_category.top()
-
-    def add_history(self, payload):
-        self.HISTORY.append(payload)
-
-    def speak_history(self, inputx, outputx):
-        x = {
-            "input" : inputx, 
-            "output": outputx
-        }
-        print(x)
-        self.speak.run(outputx)
-        self.add_history(x)
-
-    def process(self, payload):
-        print(payload)
-        if payload["success"] == True:
-            x = ""
-            if self.CURRENT_CONTEXT:
-                x = self.CURRENT_CONTEXT.respond(payload)
-
-            if x:
-                self.speak_history(payload["transcription"], x)
-                return
-
-            x = self.me_category.respond(payload)
-            if x:
-                self.CURRENT_CONTEXT = self.me_category
-                self.speak_history(payload["transcription"], x)
-                return
-
-            x = self.joke_category.respond(payload)
-            if x:
-                self.CURRENT_CONTEXT = self.joke_category
-                self.speak_history(payload["transcription"], x)
-                return
-
-            x = self.alarm_category.respond(payload)
-            if x:
-                self.CURRENT_CONTEXT = self.alarm_category
-                self.speak_history(payload["transcription"], x)
-                return
-
-            x = self.time_category.respond(payload)
-            if x:
-                self.CURRENT_CONTEXT = self.time_category
-                self.speak_history(payload["transcription"], x)
-                return
-            
-            self.speak_history(payload["transcription"], "evento no reconocido")
-        else:
-            self.speak.run(payload["error"])
+            print(e)
+            self.sTTEngine.stop()
 
     def update(self, subject, payload) -> None:
         if subject._state < 11:
-            self.process(payload)
+            print("responder", payload)
+            self.run()
 
 if __name__=='__main__':
     cristal = Cristal()
     cristal.run()
+
+
+
+
+
+
+    
