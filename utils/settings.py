f1065da3899174f75a12c69d6ed3f0b47131aff0
jose <josmejia.2401@gmail.com>
2020-10-17 16:38:45 -0500

    geolocalización

--- utils/settings.py
+++ utils/settings.py
@@ -4,7 +4,7 @@ Create key: https://openweathermap.org/appid
 """
 WEATHER_API = {
     "URL" : "http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&lang={2}&appid={3}",
-    "KEY" : "d504c812d3974c7628d4475c4eb74ff5",
+    "KEY" : "",
     "CITY": "BOGOTA",
     "UNITS": "metric",
     "LANG": "es"
@@ -12,5 +12,5 @@ WEATHER_API = {
 
 IPSTACK_API = {
     "URL" : "http://api.ipstack.com/check?access_key={}&format=1",
-    "KEY" : "5aec9b31b13f035849b504b3eb632ec0"
+    "KEY" : ""
 }
